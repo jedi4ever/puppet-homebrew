6d31e0a3aa430fa9e1374a12142ce90f5a84fa05
Patrick Debois <patrick.debois@jedi.be>
2010-12-07 10:35:54 +0100

    first commit for github

---
+++ lib/puppet/provider/package/homebrew.rb
@@ -0,0 +1,100 @@
+#################################################################################
+# Description: This file provides a puppet package provider for homebrew on Mac
+# Author: Patrick.Debois@jedi.be
+# URL: http://jedi.be/blog
+#################################################################################
+
+# Information on how to embed a provider in a module : lib/puppet/provider/package/homebrew.rb
+# http://blkperl.blogspot.com/2010/07/provider-errors.html
+
+require 'puppet/provider/package'
+
+Puppet::Type.type(:package).provide :homebrew, :parent => Puppet::Provider::Package do
+  desc "Package management using homebrew on OS X."
+
+  confine :operatingsystem => :darwin
+  commands :brewcmd => "/usr/local/bin/brew"
+
+  def self.brewlist(hash)
+    command = [command(:brewcmd), "list","--versions"]
+
+    if name = hash[:justme]
+      command << name
+    end
+    
+   begin
+       list = execute(command).split("\n").collect do |set|
+         if brewhash = brewsplit(set)
+           brewhash[:provider] = :brew
+           brewhash
+         else
+           nil
+         end
+       end.compact
+     rescue Puppet::ExecutionFailure => detail
+       raise Puppet::Error, "Could not list brews: #{detail}"
+     end
+     
+     if hash[:justme]
+       return list.shift
+     else
+       return list
+     end
+
+  end
+
+  def self.brewsplit(desc)
+      split_desc=desc.split(/ /)
+      name=split_desc[0]
+      version=split_desc[split_desc.size-1]
+      if (name.nil? || version.nil?)
+           Puppet.warning "Could not match #{desc}"
+           nil
+      else
+	      return {
+          :name => name,
+          :ensure => version
+        }
+      end 
+  end
+
+  def self.instances
+    brewlist(:local => true).collect do |hash|
+      new(hash)
+    end
+  end
+
+  def install
+    should = @resource.should(:ensure)
+
+    output = brewcmd "install", @resource[:name]
+    if output =~ /^Error: No available formula/
+      raise Puppet::ExecutionFailure, "Could not find package #{@resource[:name]}"
+    end
+  end
+
+  def query
+    version = nil
+    self.class.brewlist(:justme => resource[:name]) 
+  end
+
+  def latest
+    info = brewcmd :info, "#{@resource[:name]}"
+
+    if $CHILD_STATUS != 0 or info =~ /^Error/
+      return nil
+    end
+    version=info.split("\n")[0].split(" ")[1]
+    version
+  end
+
+  def uninstall
+    brewcmd :remove,"--force", @resource[:name]
+  end
+
+  def update
+    #when you reinstall you get the latest version
+    install
+  end
+end
+
